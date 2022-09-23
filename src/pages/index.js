1d4bf4c1f96021de8fb89cb8cdd8e4c39ef8afc9
Julian <julian.koehn1989@googlemail.com>
2022-09-23 09:21:18 +0200

    SetUpStart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import Layout from '../components/Layout';
+const logo = '/logo.png';
 
 export default function HomePage() {
 	return (
-		<Layout>
+		<>
 			<Head>
-				<title key="title">My Project</title>
+				<title key="title">My Cannabis Guide</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Head>
-			<h1>Home</h1>
-		</Layout>
+			<div>
+				<h1>Find your local expert for medical cannabis</h1>
+				<Image src={logo} alt={logo} width="100px" height="100px" />
+				<h2>Where do you want to find a cannabis expert?</h2>
+				<Link href="/search">
+					<button>enter your search here</button>
+				</Link>
+			</div>
+		</>
 	);
 }
