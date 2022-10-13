2ebfbb60aecbd5c95eb680bd3b8a615d36a01492
Julian <julian.koehn1989@googlemail.com>
2022-10-13 10:42:39 +0200

    create form

---
+++ src/components/HeaderContact.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const GreenHeader = styled.span`
+	display: flex;
+	position: fixed;
+	z-index: 10;
+	top: 0;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 90px;
+	background-color: #61adad;
+	color: white;
+	font-size: 26px;
+	font-weight: 300;
+`;
+
+export default function Header() {
+	return (
+		<GreenHeader>
+			<p>Contact</p>
+		</GreenHeader>
+	);
+}
